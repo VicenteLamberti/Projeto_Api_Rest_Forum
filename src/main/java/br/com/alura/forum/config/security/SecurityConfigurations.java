fb8febce605e1f01eb056adccc7051718ccc6fe1
Vicente Leonardo <vicente.lamberti@hotmail.com>
2022-02-21 10:56:42 -0300

    Correção nome do package config.security

--- src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
@@ -1,4 +1,4 @@
-package br.com.alura.forum.config.validacao.security;
+package br.com.alura.forum.config.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
